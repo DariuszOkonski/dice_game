19857110a598bce8e7722f4c39cd1a2c3a202e47
Dariusz Okoński <darek200180@gmail.com>
2026-01-20 06:47:46 +0100

    add Die component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Die from './Die';
 
 function App() {
   return (
     <div className='App'>
       <h1>Dice Game</h1>
-      <i className='fas fa-dice-one'></i>
+      <Die face='one' />
+      <Die face='two' />
+      <Die face='three' />
+      <Die face='four' />
+      <Die face='five' />
     </div>
   );
 }
